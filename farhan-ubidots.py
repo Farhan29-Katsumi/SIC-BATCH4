2492a4ab7f4f69945f714ce0a8c6708defc1e3d6
Maisan Hanif Dharmawan <hanif.dharmawan@gmail.com>
2023-08-26 19:14:56 +0700

    Tugas Assignment Phase 2

--- farhan-ubidots.py
+++ farhan-ubidots.py
@@ -3,20 +3,20 @@ import requests
 import math
 import random
 
-TOKEN = "BBFF-EbOfXqwIZoG6ylNinzTZaN3EUv6Bs7"  # Put your TOKEN here
-DEVICE_LABEL = "raspi-4-d-manis"  # Put your device label here 
-VARIABLE_LABEL_1 = "ultrasonic-sensor"  # Put your first variable label here
-VARIABLE_LABEL_2 = "temperature-ruangan"
-VARIABLE_LABEL_3 = "humidity"
+TOKEN = "BBFF-Qd5oSQzWdY5lraNZXwzAI924TCsiDJ"  # Put your TOKEN here
+DEVICE_LABEL = "farhan-sic"  # Put your device label here 
+VARIABLE_LABEL_1 = "noice-level"  # Put your first variable label here
+VARIABLE_LABEL_2 = "download"
+VARIABLE_LABEL_3 = "temperature-ruangan"
 
 
 def build_payload(variable_1,variable_2,variable_3):
     # Creates two random values for sending data
-    sensor_ultrasonic = int(random.randint(0 , 200))
+    db_level = int(random.randint(0 , 140))
+
+    upload_speed = int(random.randint(0, 1000))
 
     sensor_suhu = int(random.randint(0, 100))
-
-    sensor_humidity = int(random.randint(273,373))
 
     # Creates a random gps coordinates
     lat = random.randrange(34, 36, 1) + \
@@ -24,7 +24,7 @@ def build_payload(variable_1,variable_2,variable_3):
     lng = random.randrange(-83, -87, -1) + \
         random.randrange(1, 1000, 1) / 1000.0
     
-    payload = {variable_1: sensor_ultrasonic, variable_2: sensor_suhu, variable_3: sensor_humidity
+    payload = {variable_1: db_level, variable_2: upload_speed, variable_3: sensor_suhu
                }
 
     return payload
